48f1bbbc7db28c75e638f438167ae480539785cf
FranciscoSandez <134792899+FranciscoSandez@users.noreply.github.com>
2023-05-27 18:44:33 +0200

    Add files via upload

---
+++ scripts/script1-sandez.php
@@ -0,0 +1,53 @@
+<?php
+/**
+* @autor Curro
+* @version 1.0
+* @internal Script para la tarea 5 actividad 2 del módulo de Despliegue de aplicaciones web
+* @since 26/05/2023
+*/
+/**
+* Función que suma los números pasados como parámetros
+* @param int $num1 Primer número a sumar
+* @param int $num2 Segundo número a sumar
+* @return int
+*/
+function sumar($num1, $num2) {
+  return $num1 + $num2;
+}
+/** 
+* Función que resta los números pasados como parámetros 
+* @param int $num1 Número inicial 
+* @param int $num2 Número que resta al $num1 
+* @return int 
+*/ 
+
+function restar($num1, $num2) {
+  return $num1 - $num2;
+}
+/** 
+* Función que multiplica los números pasados como parámetros 
+* @param int $num1 Número inicial 
+* @param int $num2 Número que multiplica al $num1 
+* @return int 
+*/ 
+
+function multiplicar($num1, $num2) {
+  return $num1 * $num2;
+}
+
+/** 
+* Función que divide los números pasados como parámetros 
+* @param int $num1 Número inicial 
+* @param int $num2 Número que divide al $num1 
+* @return int 
+*/ 
+
+function dividir($num1, $num2) {
+  if ($num2 == 0) {
+    return "Error: No se puede dividir entre cero.";
+  } else {
+    return $num1 / $num2;
+  }
+}
+
+?>
